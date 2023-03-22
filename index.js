fa8004315b3016b1c0450f608bc274fd9d853505
Sean Kutash <seankutash@gmail.com>
2023-03-22 15:28:32 -0400

    Third commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const count = document.querySelector(".number")
+const count = document.querySelector("#number")
 let counter = 0
-const image = document.getElementById("img")
-image.addEventListener('click', function() {
+const image = document.getElementById("#img")
+image.addEventListener('click', function(myFunction) {
 	counter++
 	count.innerText = counter;	
 })
