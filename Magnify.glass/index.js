18e8646a365738de6a386bc7676c618e24424f93
Sean Kutash <seankutash@gmail.com>
2023-03-22 17:24:20 -0400

    index.js Attempt:2

--- Magnify.glass/index.js
+++ Magnify.glass/index.js
@@ -4,6 +4,9 @@ const minimize = document.querySelector("#minimize");
 minimize.addEventListener('click', function() {	
 	const currentFontSize = parseInt(getComputedStyle(p).fontSize);
 	const newFontSize = currentFontSize - 6; 
+	if (newFontSize < 1 ) {
+		newFontSize = 1;
+	}
 	p.style.fontSize = newFontSize + "px";
 })
 
@@ -11,5 +14,10 @@ const magnify = document.querySelector("#magnify")
 magnify.addEventListener('click', function() {	
 	const currentFontSize = parseInt(getComputedStyle(p).fontSize);
 	const newFontSize = currentFontSize + 6; 
+	if (newFontSize > 100) {
+		newFontSize = 100;
+	}
 	p.style.fontSize = newFontSize + "px";
 })
+
+
