d3c29da5116a9448c431d31c46433dadcae5a95d
Sean Kutash <seankutash@gmail.com>
2023-03-22 15:38:39 -0400

    Updated index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const count = document.querySelector("#number")
+// Makes sure that HTML is loaded before firing JS.
+addEventListener('DOMContentLoaded', function() {
+	const count = document.querySelector("#number")
 let counter = 0
-const image = document.getElementById("#img")
-image.addEventListener('click', function(myFunction) {
+const image = document.getElementById("img")
+
+image.addEventListener('click', function() {
 	counter++
 	count.innerText = counter;	
+	})
 })
